b9638d29cd0a0d20837958a6472450bcee7e5ad5
Randorx <Davidproxaxo@hotmail.com>
2022-12-06 13:51:12 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,56 @@
+function countdown(dateEnd) {
+  var timer, days, hours, minutes, seconds
+
+  dateEnd = new Date(dateEnd)
+  dateEnd = dateEnd.getTime()
+
+  if (isNaN(dateEnd)) {
+    return
+  }
+
+  timer = setInterval(calculate, 1000)
+
+  function calculate() {
+    var dateStart = new Date()
+    var dateStart = new Date(
+      dateStart.getUTCFullYear(),
+      dateStart.getUTCMonth(),
+      dateStart.getUTCDate(),
+      dateStart.getUTCHours(),
+      dateStart.getUTCMinutes(),
+      dateStart.getUTCSeconds(),
+    )
+    var timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000)
+
+    if (timeRemaining >= 0) {
+      days = parseInt(timeRemaining / 86400)
+      timeRemaining = timeRemaining % 86400
+      hours = parseInt(timeRemaining / 3600)
+      timeRemaining = timeRemaining % 3600
+      minutes = parseInt(timeRemaining / 60)
+      timeRemaining = timeRemaining % 60
+      seconds = parseInt(timeRemaining)
+
+      document.getElementById('days').innerHTML = parseInt(days, 10)
+      document.getElementById('hours').innerHTML = ('0' + hours).slice(-2)
+      document.getElementById('minutes').innerHTML = ('0' + minutes).slice(-2)
+      document.getElementById('seconds').innerHTML = ('0' + seconds).slice(-2)
+    } else {
+      let button = document.createElement('button')
+      let div = document.querySelector('.countdown')
+      let a = document.createElement('a')
+      a.href = './imagenes/index.html'
+      a.style.textDecoration = 'none'
+      button.textContent = 'Entrar'
+      button.style.margin = '0 auto'
+      button.className = 'bttn-one'
+      a.insertAdjacentElement('beforeend', button)
+      div.insertAdjacentElement('beforeend', a)
+      clearInterval(timer)
+    }
+  }
+
+  function display(days, hours, minutes, seconds) {}
+}
+
+countdown('12/08/2022 12:00:00 AM')
